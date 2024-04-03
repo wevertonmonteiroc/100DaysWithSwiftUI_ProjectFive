e77f3c7cc9b72ea05ac4402d96292285c9b6df30
Weverton <wevertonmonteiroc@gmail.com>
2024-04-03 19:31:56 -0300

    Word Scramble: Introduction.

---
+++ Project5_WorldScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project5_WorldScramble
+//
+//  Created by admin on 03/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project5_WorldScramble/Project5_WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project5_WorldScrambleApp.swift
+//  Project5_WorldScramble
+//
+//  Created by admin on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Project5_WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
